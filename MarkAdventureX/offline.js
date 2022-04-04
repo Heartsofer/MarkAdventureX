3eaf6eebf6acb862842b3af7c6ceca23cd74cd27
Heartsofer <102953716+Heartsofer@users.noreply.github.com>
2022-04-05 01:29:49 +0700

    MarkAdventureX
    HardGame

---
+++ MarkAdventureX/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1649096682,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/thebackground2819000_-sheet0.png",
+		"images/kisspngchameleonfallstwodimensionalspaceplatformgambearmoves5b1d5d2b3712f-sheet0.png",
+		"images/kisspngchameleonfallstwodimensionalspaceplatformgambearmoves5b1d5d2b3712f-sheet1.png",
+		"images/player-sheet0.png",
+		"images/gold-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/player1-sheet0.png",
+		"media/yt1s.com-my-hero-academia-ost-you-say-run-jet-set-run-you-say-run-v2.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
